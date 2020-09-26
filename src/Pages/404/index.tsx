8b0de2d9ee2bdc7a3f261ca087fe59873f155d39
EdgarJFA <62776977+EdgarJFA@users.noreply.github.com>
2020-09-27 00:22:53 +0200

    Update NotFound Page Sep 27

--- src/Pages/404/index.tsx
+++ src/Pages/404/index.tsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 import './styles.css';
 
 function NotFound() {
+    const [time, setTime] = useState(30);
+    const History = useHistory();
+
+    function handleReloadTime(time: number) { 
+        if(time > 1) {             
+            setTime(time-1);
+        } else {
+            // Reload da pagina getCoutries
+            History.push('/listofcountries');
+        }
+    }
+
+    // function HandleReload() {
+    //     setTime(0);
+    //     History.push('/listofcountries');
+    // }
+
+    useEffect(() => {
+        setTimeout(                
+            () => {handleReloadTime(time)}
+        , 1000);        
+    }, [time]);
+
     return (
-        <div>
-            <h1>Page Not Found</h1>
+        <div id="PageNotFound">
+            <div className="content">
+                <header>
+                    <Link to="/" rel="noopener noreferrer">Home</Link>
+                    <Link to={{ pathname: "https://github.com/apilayer/restcountries" }} target="_blank" rel="noopener noreferrer">View on Github</Link>
+                    <Link to={{ pathname: "https://restcountries.eu/#rest-countries" }} target="_blank" rel="noopener noreferrer">Docs API</Link>
+                    <Link to="/listofcountries" rel="noopener noreferrer">Get Countries</Link>
+                    {/* <a href="http://" target="_blank" rel="noopener noreferrer">Contact US</a> */}
+                </header>
+                <main>
+                    <div className="TextContent">
+                        <h2>We have a problem</h2>
+                        <p>Page not found</p>
+                        <Link to="/listofcountries" className="normal">Reload {(time<=0)? '': time}</Link>
+                    </div>
+                </main>
+            </div>
         </div>
-    )
+    );
 }
 
 export default NotFound;
