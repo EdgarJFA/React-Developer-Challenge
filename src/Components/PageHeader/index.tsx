e12b9e0593f5fc500dc8ecc71b73124e6481409b
EdgarJFA <62776977+EdgarJFA@users.noreply.github.com>
2020-09-26 21:41:36 +0200

    Update PageHeader

---
+++ src/Components/PageHeader/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+function PageHeader() {
+    return (
+        <div>
+            <h2>Header Menu</h2>            
+        </div>
+    );
+}
+
+export default PageHeader;
